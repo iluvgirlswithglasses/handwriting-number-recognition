da9eaa9de7a873947b8874268839d549d6eac1ac
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-12-02 14:31:44 +0700

    submodule & build files

---
+++ param.h
@@ -0,0 +1,39 @@
+
+/*
+author: 	iluvgirlswithglasses
+github: 	https://github.com/iluvgirlswithglasses
+created:	Sat Dec  2 14:31:22 2023
+style:  	C-Kernel with custom pointer rule
+tab-width:	4 spaces
+
+ /\_/\
+( o.o )
+ > ^ <
+
+I firmly believe in the supremacy of the Euphonium
+FYI I use Debian
+*/
+
+#ifndef PARAM_H
+#define PARAM_H
+
+/** @ docs */
+
+/** @ dependencies */
+
+/** @ implementation */
+namespace NumberRecog
+{
+
+struct Param
+{
+public:
+
+private:
+
+};
+
+}	// namespace NumberRecog
+
+#endif	// PARAM_H
+
